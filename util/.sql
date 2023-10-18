6b810a7ae0fd77d18ccc375f61a3de7f58e7a2c5
Davasz <davidavi200615@gmail.com>
2023-10-17 22:36:14 -0300

    ⬆️UPDATE: SQL file

--- util/.sql
+++ util/.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE auth_jwt;
+
+CREATE TABLE `auth_jwt`.`users` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(70) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
