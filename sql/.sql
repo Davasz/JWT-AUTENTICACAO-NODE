616b4c97d545805e7e0500dd42e11520d72f4dcd
Davasz <davidavi200615@gmail.com>
2023-10-22 18:13:21 -0300

    🔧chore: Configuração do banco

--- sql/.sql
+++ sql/.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE auth_jwt;
 
-CREATE TABLE `auth_jwt`.`new_table` (
+CREATE TABLE `auth_jwt`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(65) NOT NULL,
   PRIMARY KEY (`id`));
