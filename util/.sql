cba96795316fb530b6d8de282da3d234a57b993e
Davasz <davidavi200615@gmail.com>
2023-10-22 17:54:28 -0300

    🔥remove

--- util/.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE auth_jwt;
-
-CREATE TABLE `auth_jwt`.`new_table` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
